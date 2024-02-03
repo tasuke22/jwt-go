b025b5f4129ace81926999f605c92c70f331a2b6
tasuke22 <tasukesuke@gmail.com>
2024-02-03 13:59:47 +0900

    initial commit

---
+++ Makefile
@@ -0,0 +1,14 @@
+.PHONY: help
+
+help:
+	@echo "\033[32mAvailable targets:\033[0m"
+	@grep "^[a-zA-Z\-]*:" Makefile | grep -v "grep" | sed -e 's/^/make /' | sed -e 's/://'
+
+cp-env:
+	cp .env.example .env
+up:
+	docker compose up -d
+db:
+	docker compose exec -it db mysql -u myuser -pmypassword -D mydatabase
+logs:
+	docker compose logs -f
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arrayList [5]int
+	for _, v := range arrayList {
+		fmt.Println(v)
+	}
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+}
